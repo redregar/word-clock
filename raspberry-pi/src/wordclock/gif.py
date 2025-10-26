688cb119ad6d415b67bc69117da94b74d6e70a57
redregar <frankweigert@gmail.com>
2025-10-26 19:01:01 +0100

    Update gif.py
    gut optimzed version

--- raspberry-pi/src/wordclock/gif.py
+++ raspberry-pi/src/wordclock/gif.py
@@ -3,35 +3,45 @@ import time
 from clock_display_hal import ClockDisplayHAL
 
 
-def update_led_pixels(gif_pixels, new_size, clock_display_hal):
+def update_led_pixels(gif_pixels, new_size, clock_display_hal, background_color=(0, 0, 0)):
+    """Setzt alle Pixel des Frames auf der Wortuhr."""
     for y in range(new_size[1]):
         for x in range(new_size[0]):
-            r, g, b = gif_pixels[x, y][:3]
-            clock_display_hal.set_pixel(x, y, (r, g, b))
+            pixel = gif_pixels[x, y]
+            if len(pixel) == 4:
+                r, g, b, a = pixel
+                color = (r, g, b) if a > 0 else background_color
+            else:
+                r, g, b = pixel[:3]
+                color = (r, g, b)
+            clock_display_hal.set_pixel(x, y, color)
 
 
-def display_gif(gif_path, clock_display_hal, display_gif_duration=4, background_color=(0, 0, 0)):
+def display_gif(
+    gif_path,
+    clock_display_hal,
+    display_gif_duration=4,
+    background_color=(0, 0, 0)
+):
+    """Zeigt ein GIF auf der Wortuhr an, berücksichtigt Frametiming und Transparenz."""
     img = Image.open(gif_path)
     new_size = (ClockDisplayHAL.WIDTH, ClockDisplayHAL.HEIGHT)
     start_time = time.time()
 
     while time.time() < start_time + display_gif_duration:
         try:
-            frame = img.convert("RGBA")
-            frame = frame.resize(new_size)
+            frame = img.convert("RGBA").resize(new_size, resample=Image.NEAREST)
             gif_pixels = frame.load()
-
-            for y in range(new_size[1]):
-                for x in range(new_size[0]):
-                    r, g, b, a = gif_pixels[x, y]
-                    if a > 0:
-                        clock_display_hal.set_pixel(x, y, (r, g, b))
-                    else:
-                        clock_display_hal.set_pixel(x, y, background_color)
-
+            update_led_pixels(gif_pixels, new_size, clock_display_hal, background_color)
             clock_display_hal.show()
-            time.sleep(0.1)
+
+            # GIF-spezifisches Framedelay
+            delay_ms = img.info.get('duration', 100)  # Default 100 ms
+            time.sleep(delay_ms / 1000)
+
+            # Nächstes Frame
             img.seek(img.tell() + 1)
 
         except EOFError:
+            # Zurück zum ersten Frame
             img.seek(0)
